65aa0de6ff9ca5a66ab8f679e9b141cc67be8aa9
Dennys Rocha <dennysrocha@iMac-de-Dennys.local>
2021-05-08 16:25:36 -0300

    Nice solution but it crashes due to depth limit... Need other type of sol

--- python/1202.py
+++ python/1202.py
@@ -2,25 +2,56 @@ import math
 
 def subtractOne(n):
     aux = len(n)
-    while n[aux-1] == "0":
+    if aux == 1:
+        return "0"
+    while n[aux-1] == "0": # check bits till find "1"
         aux -= 1
-    n[aux-1] = "0"
-    while aux != len(n):
-        n[aux] = "1"
+    
+    if aux == 1: # case that all numbers == "1", except first
+        res = "1"
+        for _ in range(len(n)-2):
+            res += "1"
+        return res
+    
+    n = n[:aux-1] + "0" + n[aux:]
+    while aux != len(n): # changing "0" to "1" till last bit
+        n = n[:aux] + "1" + n[aux+1:]
+        aux += 1
+
+    return n
+
+def sumOne(n):
+    aux = len(n)
+    if aux == 1: 
+        return "10"
+    while n[aux-1] == "1" and aux > 1: # check bits till find "0"
+        aux -= 1
+    
+    if aux == 1: # case that all numbers == "1"
+        res = "1"
+        for _ in range(len(n)):
+            res += "0"
+        return res
+
+    n = n[:aux-1] + "1" + n[aux:]
+    while aux != len(n): # changing "1" to "0" till last bit
+        n = n[:aux] + "0" + n[aux+1:]
+        aux += 1
+    
     return n
 
 def fib(n):
-    if n == "0" or n == "1":
+    if n == "1" or n == "10":
         return 1
-    aux = len(n) - 1 # index of last bit
-    if n[aux] == "0":
-        a = fib(n[:aux+1])
-        b = fib(subtractOne(n[:aux+1]))
+    aux = len(n)
+    if n[aux-1] == "0": # par
+        a = fib(n[:aux-1]) # remove one bit
+        b = fib(subtractOne(n[:aux-1])) # remove one bit and subtract "1"
         return a*(2*b + a)
-    else:
-        # TO-DO
-        # need to implement sumOne(n)
-        return
+    else: # impar
+        a = fib(sumOne(n[:aux-1])) # remove one bit and sum "1"
+        b = fib(n[:aux-1]) # remove one bit
+        return a*a + b*b
 if __name__ == '__main__':
 # first, see that 0 < n < 10^301 (yep, n can be f*cking large), so be careful with laces/recursivity
 # due to it, we'll use "Double Fibonacci Identitie":
@@ -29,8 +60,4 @@ if __name__ == '__main__':
     T = int(input())
     for _ in range(T):
         n = input()
-        while n[0] == "0":
-            n = n[1:]
-        print(fib(n))
-
-
+        print(str(fib(n)%1000).zfill(3))
