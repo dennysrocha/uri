149c118eedbdd5aad86ffedc9985a349b2e34844
Dennys Rocha <dennysrocha@iMac-de-Dennys.local>
2021-05-08 23:06:18 -0300

    Solution using mod 1500 and Double Fibonacci Identitie

--- python/1202.py
+++ python/1202.py
@@ -1,63 +1,36 @@
-import math
-
-def subtractOne(n):
-    aux = len(n)
-    if aux == 1:
-        return "0"
-    while n[aux-1] == "0": # check bits till find "1"
-        aux -= 1
-    
-    if aux == 1: # case that all numbers == "1", except first
-        res = "1"
-        for _ in range(len(n)-2):
-            res += "1"
-        return res
-    
-    n = n[:aux-1] + "0" + n[aux:]
-    while aux != len(n): # changing "0" to "1" till last bit
-        n = n[:aux] + "1" + n[aux+1:]
-        aux += 1
-
-    return n
-
-def sumOne(n):
-    aux = len(n)
-    if aux == 1: 
-        return "10"
-    while n[aux-1] == "1" and aux > 1: # check bits till find "0"
-        aux -= 1
-    
-    if aux == 1: # case that all numbers == "1"
-        res = "1"
-        for _ in range(len(n)):
-            res += "0"
-        return res
-
-    n = n[:aux-1] + "1" + n[aux:]
-    while aux != len(n): # changing "1" to "0" till last bit
-        n = n[:aux] + "0" + n[aux+1:]
-        aux += 1
-    
-    return n
-
 def fib(n):
-    if n == "1" or n == "10":
+    if n<3:
         return 1
-    aux = len(n)
-    if n[aux-1] == "0": # par
-        a = fib(n[:aux-1]) # remove one bit
-        b = fib(subtractOne(n[:aux-1])) # remove one bit and subtract "1"
+
+    a = fib(n//2)
+    if n%2==0:
+        b = fib((n//2) - 1)
         return a*(2*b + a)
-    else: # impar
-        a = fib(sumOne(n[:aux-1])) # remove one bit and sum "1"
-        b = fib(n[:aux-1]) # remove one bit
+    else:
+        b = fib((n//2) + 1)
         return a*a + b*b
+
+
 if __name__ == '__main__':
 # first, see that 0 < n < 10^301 (yep, n can be f*cking large), so be careful with laces/recursivity
-# due to it, we'll use "Double Fibonacci Identitie":
+# due to it, we will need to find k such that fib(k+1)%1000 == 1, fib(k+2) == 1, then the new sequence
+# starting at k will be the same as starting from 1...
+# after some coding:
+# 
+# a, b = 1, 1
+# k = 2
+# while True:
+#     a, b = b, a+b
+#     if a%1000 == 1 and b%1000 == 1:
+#         print(k-1)
+#         break
+#     k += 1
+# 
+# The result is k=1500.
+# To make the solution faster as possible, we will use "Double Fibonacci Identitie":
 # F(2n) = F(n)*(2*F(n-1) + F(n))
 # F(2n+1) = F(n+1)^2 + F(n)^2
     T = int(input())
     for _ in range(T):
-        n = input()
+        n = int(input(),2)%1500
         print(str(fib(n)%1000).zfill(3))
