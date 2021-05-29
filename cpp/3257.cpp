a4021a4bfca435865a13efe366b3991763d08217
Dennys Rocha <dennysrocha@iMac-de-Dennys.local>
2021-05-29 16:06:38 -0300

    Solution with quickSort... Bubble wasn't enough rs

--- cpp/3257.cpp
+++ cpp/3257.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main() {
-    int N, T, res, counter;
-    res = -1;
-    counter = 1;
-    cin >> N;
-    while(N--) {
-        cin >> T;
-        if(T==res) counter++;
-        else if(T>res) {
-            res = T;
-            counter = 1;
+// check this amazing video https://bit.ly/3c4FmKV... be happy :P
+void quickSort(int T[], int left, int right) {
+    int i, j, pivot, aux;
+    i = left;
+    j = right-1;
+    // cout << ((left+right)>>1) << endl;
+    pivot = T[(left+right)>>1];
+    while(i <= j) {
+        while(T[i] > pivot && j < right) i++;
+        while(T[j] < pivot && j > left) j--;
+        if(i <= j) {
+            aux = T[i];
+            T[i] = T[j];
+            T[j] = aux;
+            i++;
+            j--;
         }
     }
-    cout << res + counter << endl;
+    if(j > left) quickSort(T, left, j+1);
+    if(i < right) quickSort(T, i, right);
+}
+
+int main() {
+    int N, res, days;
+    cin >> N;
+    int T[N];
+    for(int i=0; i<N; i++) cin >> T[i];
+    quickSort(T, 0, N);
+    res = 0;
+    for(int i=0; i<N; i++) {
+        days = T[i] + (i+1) + 1;
+        if(res < days) res = days;
+    }
+    cout << res << endl;
+    // cout << res + counter << endl;
 }
